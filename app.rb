89bd3ddb7e006aa3da9824ea4366acd564c63dbe
Andrew Ritchie <ritchiea@gmail.com>
2012-09-26 14:53:47 -0400

    added routed, refactored model, focus on admin features, removed some sinatra routes

--- app.rb
+++ app.rb
@@ -9,23 +9,15 @@ get '/' do
   # homepage
 end
 
-get '/posts/:id' do
-  # single post
-end
-
 get '/admin' do
   # cms
 end
 
-post '/admin' do
-  # save post
-end
-
-get '/api/:thing' do
+get '/admin/api/:thing' do
   DB.collection(params[:thing]).find.to_a.map{|t| from_bson_id(t)}.to_json
 end
 
-get '/api/:thing/:id' do
+get '/admin/api/:thing/:id' do
   from_bson_id(DB.collection(params[:thing]).find_one(to_bson_id(params[:id]))).to_json
 end
 
--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,21 @@
 
 $(function(){
 
+  // backbone router
+
+  var BlogRouter = Backbone.Router.extend({
+  routes: { "posts/:id": "route" },
+
+  route: function(id) {
+    var item = PostArchive.get(id);
+    var view = new AdminView({ model: item });
+
+    something.html( view.render().el );
+  }
+  });
+
   // posts model
-  // posts should have text, a title, an author, a boolean published status,
-  // and 
+  // posts should have text, a title, an author & a boolean published status
 
   window.Post = Backbone.Model.extend({
     idAttribute: "_id",
@@ -12,9 +24,23 @@ $(function(){
     defaults: function() {
       return {
         author: "Andrew Ritchie",
+        title: "New Post",
+        text: "Hello world",
         published:  false,
         order: Posts.nextOrder()
       };
+    },
+
+    initialize: function(){
+        console.log("this model has been instantiated");
+        this.on("change:text", function(){
+            var text = this.get("text");
+            console.log('Text source updated');
+        });
+    },
+
+    changeText: function( posttext ){
+        this.set({ text: posttext });
     },
 
     // Toggle the 'published' state of this todo item.
@@ -25,12 +51,12 @@ $(function(){
 
   });
 
-    // blogposts collection
+  // blogposts collection
 
-  var PostList = Backbone.Collection.extend({
+  var PostArchive = Backbone.Collection.extend({
 
   model: Post,
-  url: 'api/posts',
+  url: 'admin/api/posts',
 
   // keep unpublished posts in sequential order
   
@@ -41,14 +67,47 @@ $(function(){
 
   });
 
-  var Posts = new PostList;
+  var Posts = new PostArchive;
+
+  // views
+
+  var PostView = Backbone.View.extend({
+
+    // templating function
+    template: _.template($('#post-template').html()),
+
+    // The DOM events specific to an item.
+    events: {
+      "click publish" : "togglePublished"
+    },
+
+    // listen for changes to posts model
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.on('change', this.render);
+      this.model.on('destroy', this.remove);
+    },
+
+    // Re-render the post
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    // Toggle the "published" state of the model.
+    togglePublished: function() {
+      this.model.toggle();
+    }
+
+  });
 
   // My blog App
   // top level piece of UI
 
-  window.AppView = Backbone.View.extend({
+  window.AdminView = Backbone.View.extend({
 
-  el: $("#andrew-blog"),
+  el: $("#admin"),
 
   initialize: function() {
     Posts.on('all',   this.render, this);
@@ -60,6 +119,6 @@ $(function(){
 
   // create the app
 
-  window.App = new AppView;
+  window.App = new BlogView;
 
 }); // app end
